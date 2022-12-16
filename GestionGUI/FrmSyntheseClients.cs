83ee6d48177d1af0b26e23550f6e2a85e65b1481
FeryKevin <kevin.fery@outlook.com>
2022-12-16 09:43:47 +0100

    filtre

--- GestionGUI/FrmSyntheseClients.cs
+++ GestionGUI/FrmSyntheseClients.cs
@@ -1,6 +1,8 @@
 ﻿using GestionBLL;
 using GestionBO;
 using System.Windows.Forms;
+using Connexion;
+
 
 namespace GestionGUI
 {
@@ -55,6 +57,10 @@ namespace GestionGUI
                     prAtt = (nbAtt / taille) * 100;
                 }
 
+                prAcc = Math.Round(prAcc, 1, MidpointRounding.ToEven);
+                prRef = Math.Round(prRef, 1 , MidpointRounding.ToEven);
+                prAtt = Math.Round(prAtt, 1, MidpointRounding.ToEven);
+
                 dataGridSynthese.Rows.Add(cli.Nom, taille, nbAcc, prAcc + "%", prAtt + "%", prRef + "%", prixTotalDevis);
             }
 
@@ -71,14 +77,13 @@ namespace GestionGUI
         }
 
         private void deconnexion_Click(object sender, EventArgs e)
-        {
-            /*
+        {            
             this.Hide();
-            ConnexionForm ConnexionForm;
-            ConnexionForm = new ConnexionForm();
-            ConnexionForm.Closed += (s, args) => this.Close();
-            ConnexionForm.ShowDialog(); // déconnexion
-            this.Close();*/
+            ConnexionForm connexionForm;
+            connexionForm = new ConnexionForm();
+            connexionForm.Closed += (s, args) => this.Close();
+            connexionForm.ShowDialog(); // déconnexion
+            this.Close();
         }
 
         private void btnClient_Click(object sender, EventArgs e)
@@ -110,8 +115,67 @@ namespace GestionGUI
         {
             DateTime date1 = DateTime.Parse(dateTimeSynth.Value.ToString());
             DateTime date2 = DateTime.Parse(dateTimeSynth2.Value.ToString());
+            dataGridSynthese.Rows.Clear();
 
 
+            foreach (Client cli in ClientBLL.GetClient())
+            {
+                List<Devis> listDevis = cli.LesDevis;
+                int code_devis;
+                double prixTotalDevis = 0;
+                double nbAcc = 0, nbRef = 0, nbAtt = 0;
+
+                foreach (Devis dev in listDevis)
+                { 
+                    if (DateTime.Compare(date1, dev.Date) <= 0 && DateTime.Compare(dev.Date, date2) <= 0)
+                    {
+                        switch (dev.Statut.Libelle)
+                        {
+                            case "Accepté":
+                                nbAcc++;
+                                break;
+                            case "Refusé":
+                                nbRef++;
+                                break;
+                            case "En attente":
+                                nbAtt++;
+                                break;
+                            default:
+                                break;
+                        }
+
+                        code_devis = dev.Code;
+                        foreach (Contenir con in ContenirBLL.GetConteneurs(code_devis))
+                        {
+                            double prixTotal = con.Produit.Prix * con.Quantite;
+                            // prixTotalDevis = prixTotalDevis + prixTotal;
+                            prixTotalDevis = prixTotalDevis + (prixTotal - (prixTotal * (con.Remise / 100)));
+
+                            prixTotalDevis = Math.Round(prixTotalDevis, 1, MidpointRounding.ToEven);
+                        }   
+
+                    }               
+                }
+
+                double prAcc = 0, prRef = 0, prAtt = 0;
+                double taille = nbAcc + nbAtt + nbRef;
+
+
+                if (taille > 0)
+                {
+                    prAcc = (nbAcc / taille) * 100;
+                    prRef = (nbRef / taille) * 100;
+                    prAtt = (nbAtt / taille) * 100;
+
+                    prAcc = Math.Round(prAcc, 1, MidpointRounding.ToEven);
+                    prRef = Math.Round(prRef, 1, MidpointRounding.ToEven);
+                    prAtt = Math.Round(prAtt, 1, MidpointRounding.ToEven);
+
+                    dataGridSynthese.Rows.Add(cli.Nom, taille, nbAcc, prAcc + "%", prAtt + "%", prRef + "%", prixTotalDevis);
+                }              
+
+                
+            }
         }
     }
 }
